978c3fa978525b5ae01396546ed0ed2cbbaf0859
rimrachai-marma <rimrachai02@gmail.com>
2025-07-23 03:49:26 +0600

    Force HTTPS asset URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -12,6 +13,8 @@ class AppServiceProvider extends ServiceProvider {
 
 
     public function boot(): void {
-        //
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
